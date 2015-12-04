03075f9ed02b06abe2ae5279ce5afd5a3d305e7a
Jennifer Hunter <j.hunter@gatech.edu>
2015-12-04 16:18:23 -0500

    Added Functionality to the Score Text
    - Added a script to the score GUI text that allows you to call
    addScore(int) in order to increment the score

---
+++ Assets/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	public GUIText scoreText;
+	private static int score = 0;
+	
+	public void Update ()
+	{
+		scoreText.text = "SCORE: " + score + "pts";
+	}
+	
+	public static void addScore (int increment)
+	{
+		score += increment;
+	}
+	
+	
+}
