4fd92a34e84f1be1788203d41c551cd80af161bb
Jennifer Hunter <j.hunter@gatech.edu>
2015-12-04 13:13:32 -0500

    Added Left Flipper Functionality - Left Flipper now adds force to the ball when the Z key is pressed (this can easily be changed to be dependent on the rotation amount or the force applied to the physical controller) - Left Flipper rotates around left end

--- Assets/rotateLeftFlipper.cs
+++ Assets/rotateLeftFlipper.cs
@@ -7,10 +7,10 @@ public class rotateLeftFlipper : MonoBehaviour {
 	private Vector3 currEuler; // Current Euler angle
 	private Vector3 startEuler; // Starting Euler angle
 	private Vector3 destEuler = Vector3.zero; // Destination Euler angle
-	public float rotAmount = -90.0f; // Amount to rotate by
-	public float speed = 100.0f;
-	//private int reset = 20; // Frames to wait before resetting flipper
+	public float rotAmount = 60.0f; // Amount to rotate by - dependent on force of flipper
+	private int reset = 15; // Frames to wait before resetting flipper
 	private Transform pivot; // left pivot point
+	private bool down = true; // flag to see if the flipper is down
 
 	// Use this for initialization
 	void Start () {
@@ -21,34 +21,49 @@ public class rotateLeftFlipper : MonoBehaviour {
 		currEuler = transform.eulerAngles;
 
 		// Set reference to the pivot point
-		//pivot = transform.Find("Left Pivot Point");
+		pivot = transform.Find("Left Pivot Point");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Z)) {
-
-			// Rotate by the desired amount in the z-axis
-			destEuler.z = rotAmount;
-			// Adjust the euler angles for the object
-			currEuler = Vector3.Lerp(currEuler, destEuler, Time.deltaTime * speed);
-			transform.eulerAngles = currEuler;
-
-			// Verify that the position hasn't changed
+		// For testing purposes, I'm using the Z key instead of a dynamic rotation amount to trigger the flipper
+		if (Input.GetKeyDown (KeyCode.Z) && down) {
+			// Rotate the flipper by the desired amount
+			transform.Rotate(Vector3.forward * rotAmount);
+			// Do maths to reposition the flipper
+			// It rotates around the center, not the left end
+			float pos = Mathf.Abs(transform.position.x - pivot.position.x);
+			float diffY = Mathf.Abs(transform.position.y - pivot.position.y);
+			transform.position = new Vector3(transform.position.x - pos - Mathf.Abs(pos * Mathf.Cos (rotAmount * 3.1415926f / 180.0f)),
+			                                 transform.position.y + Mathf.Abs(pos * Mathf.Sin (rotAmount * 3.1415926f / 180.0f)),
+			                                 transform.position.z);
+			transform.Translate(Vector3.left * Mathf.Abs(pos - Mathf.Abs(pos * Mathf.Cos (rotAmount * 3.1415926f / 180.0f))));
+			transform.Translate(Vector3.up * Mathf.Abs(diffY -Mathf.Abs(pos * Mathf.Sin (rotAmount * 3.1415926f / 180.0f))));
+			// Mark that the flipper is up
+			down = false;
+			// Start the reset counter
+			reset = 15;
+		} else if (reset == 0) {
+			//Reset angle and position
 			transform.position = startPos;
-			//transform.RotateAround(pivot.position, Vector3.right, rotAmount);
-			//reset = 20;
-		} else {
-			// Reset angle
-			// Reset angle
-			currEuler = Vector3.Lerp(currEuler, startEuler, Time.deltaTime * speed);
-			transform.eulerAngles = currEuler;
-		} /**else if (reset == 20) {
-
+			transform.rotation = startRot;
+			transform.eulerAngles = startEuler;
+			// Reset reset counter
+			reset = 15;
+			// Mark that the flipper has been reset
+			down = true;
 		} else {
 			// Decrement frame wait
 			reset--;
 		}
-		**/
+	}
+
+	// Detect Collisions with the Ball
+	void OnCollisionStay (Collision col)
+	{	// If the object is a ball, bounce it with a force dependent on the rotation amount (the variable changed by the controller)
+		if(col.gameObject.tag == "Ball" && !down)
+		{
+			col.rigidbody.AddForce(Vector3.up * rotAmount *4, ForceMode.Acceleration);
+		}
 	}
 }
