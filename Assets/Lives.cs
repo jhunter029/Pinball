ebcc1d3ffaabb8f80f75f200abb7af1fcff195d4
Jennifer Hunter <j.hunter@gatech.edu>
2015-12-04 16:22:54 -0500

    Added Lives GUI Text Functionality
    - Added script to the lives remaining GUI text that allows you to
    decrement the remaining lives by calling loseLife()

---
+++ Assets/Lives.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lives : MonoBehaviour {
+	public GUIText lifeText;
+	private static int lives = 3;
+	
+	public void Update ()
+	{
+		lifeText.text = "Lives Remaining: " + lives;
+	}
+	
+	public static void loseLife ()
+	{
+		lives--;
+	}
+}
