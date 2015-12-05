1dfe9c53ad84862d9e2268bf1bc4a321d5c57b25
Jennifer Hunter <j.hunter@gatech.edu>
2015-12-04 22:17:18 -0500

    Added Points for Hitting Obstacles/Bumpers
    - Added a new obstacle (ramp on the left = 50 pts)
    - Added points for hitting each of the obstacles (easiest to hit = 10
    pts, others = 20 pts)

---
+++ Assets/Bumper20.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper20 : MonoBehaviour {
+	// Detect Collisions with the Ball
+	void OnCollisionEnter (Collision col)
+	{	// If the object is a ball, bounce it with a force dependent on the rotation amount (the variable changed by the controller)
+		if(col.gameObject.tag == "Ball")
+		{
+			Score.addScore(10);
+		}
+	}
+}
---
+++ Assets/Obstacle50.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle50 : MonoBehaviour {
+	// Detect Collisions with the Ball
+	void OnCollisionEnter (Collision col)
+	{	// If the object is a ball, bounce it with a force dependent on the rotation amount (the variable changed by the controller)
+		if(col.gameObject.tag == "Ball")
+		{
+			Score.addScore(50);
+		}
+	}
+}
