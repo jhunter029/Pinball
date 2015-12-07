36bac99e7ebda62c9df01bed1fdc4c4945c03fde
Jennifer Hunter <j.hunter@gatech.edu>
2015-12-07 15:08:25 -0500

    Added Tilt Functionality
    - Camera and gravity change according to the tilt variable

---
+++ Assets/TiltCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltCamera : MonoBehaviour {
+	private Quaternion startRot;
+	// Use this for initialization
+	void Start () {
+		startRot = transform.rotation;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(0, 0, 90 * PinballSerial.tilt);
+		Physics.gravity = new Vector3(0, -9.81f * (1 - Mathf.Abs(PinballSerial.tilt)), -9.81f * PinballSerial.tilt);
+		print (Physics.gravity);
+	}
+}
