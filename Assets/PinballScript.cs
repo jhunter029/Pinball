b4013a1da14fc1efda37651c49e847a9a5c6c9aa
Jennifer Hunter <j.hunter@gatech.edu>
2015-12-07 12:32:58 -0500

    Plunger - Reads in Slide Potentiometer Values
    - Switched from space bar to using the slide potentiometer to dictate
    when to plunge the ball
    - While loop to wait until the ball is pulled back to the max is a
    little buggy

--- Assets/PinballScript.cs
+++ Assets/PinballScript.cs
@@ -8,9 +8,9 @@ public class PinballScript : MonoBehaviour {
     
     // Physics Variables
 	private Rigidbody rb; // game object's rigidbody reference
-	public int thrust; // amount of force for plunger
+	private int maxThrust = 1400/4; // max thrust ball can receive without going haywire
 	private  Vector3 startPos; // the starting position
-	private bool plunger; // Bool to determine whether or not to add plunger force
+	private int max; // max value of that plunger set
 	private bool gameOver = false; // If the ball is out of play (wait for coin)
 	// Use this for initialization
 	void Start () {
@@ -18,19 +18,21 @@ public class PinballScript : MonoBehaviour {
 		startPos = transform.position;
 		// Set the rigidbody reference variable
 		rb = GetComponent<Rigidbody>();
-		// Designate that the ball starts in the plunger area
-		plunger = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!gameOver) {
 			// Check if the ball is in the plunger area or dead
-			if (Input.GetKeyDown (KeyCode.Space) && transform.position.x < 0.1f &&
-				transform.position.y < 9.1f && plunger) {
+			if (PinballSerial.plunger > 1 && transform.position.x < 0.1f &&
+				transform.position.y < 9.1f) {
+				max = 1;
+				// Wait for the max value to be met - allows people to pull back slowly
+				while(max <= PinballSerial.plunger) {
+					max = PinballSerial.plunger;
+				}
 				// Apply upward force on the ball
-				rb.AddForce (transform.up * thrust);
-				plunger = false;
+				rb.AddForce (transform.up * maxThrust * max);
 			} else if (transform.position.y < 3.0f) {
 				// If the ball is dead, deduct from total lives
 				Lives.loseLife ();
@@ -41,8 +43,6 @@ public class PinballScript : MonoBehaviour {
 					rb.angularVelocity = new Vector3 (0, 0, 0);
 					// Make new ball at plunger area
 					Instantiate (rb, startPos, Quaternion.identity);
-					// Turn on the plunger
-					plunger = true;
 				}
 				// Destroy current ball
 				Destroy (gameObject);
